cba537f2a2050ad60867ea737bcfdca930282016
Tomasz Siuta <tomaszsiuta@wp.pl>
2018-05-19 13:53:02 +0200

    3komentarz

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,16 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+  <h1>Twój e-mail to {{ email }}</h1>
+
+
+  <div v-if="email.length < 10">Ale masz krótki adres!</div>
+  <div v-else-if="email.length < 15">Twój adres e-mail jest w sam raz.</div>
+  <div v-else>Twój adres e-mail jest stanowczo za długi.</div>
+
+    <input type="email" v-model="email">
+
+    <button @click="alertMyEmail()">Wyświetl mój e-mail w alercie</button>
+
   </div>
 </template>
 
@@ -24,7 +19,7 @@ export default {
   name: 'app',
   data () {
     return {
-      msg: 'Hello World!!!'
+      email: 'email@email.pl'
     }
   }
 }
